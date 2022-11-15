169c346f59c1af550c7a536e5743e22985732274
hrxiang <xianghairui@126.com>
2022-11-15 15:32:12 +0800

    更新注册登录流程

--- lib/src/pages/mine/setup_username/setup_name_logic.dart
+++ lib/src/pages/mine/setup_username/setup_name_logic.dart
@@ -2,13 +2,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_openim_sdk/flutter_openim_sdk.dart';
 import 'package:get/get.dart';
 
+import '../../../common/apis.dart';
+import '../../../widgets/loading_view.dart';
+
 class SetupUserNameLogic extends GetxController {
   var inputCtrl = TextEditingController();
 
   void setupName() async {
-    await OpenIM.iMManager.userManager.setSelfInfo(
-      nickname: inputCtrl.text,
-    );
+    await LoadingView.singleton.wrap(
+        asyncFunction: () => Apis.updateUserInfo(
+              userID: OpenIM.iMManager.uid,
+              nickname: inputCtrl.text,
+            ));
     Get.back(result: inputCtrl.text);
   }
 
